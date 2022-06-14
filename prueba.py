b898b8c4c07d9d4fa241b41f3d780fe37afbfb17
carlos <carlos.garcia642@alu.uhu.es>
2022-06-14 18:06:38 +0200

    Refresh antes de grabar video

--- prueba.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-print('Version de TENSORFLOW: ' + tf.__version__)
-print('=======================================================================================================================================')
-print('Configuracion de GPU:')
-tf.config.list_physical_devices('GPU')
