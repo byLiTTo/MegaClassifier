73ef47da5f82b828f5736e95d3875364d692ccc2
carlosgarsil98@hotmail.com <carlosgarsil98@hotmail.com>
2022-09-21 10:16:41 +0200

    Move to DataSpell IDE

--- modules/aplica_mascaras.py
+++ modules/aplica_mascaras.py
@@ -16,12 +16,9 @@ import traceback
 import statistics
 import humanfriendly
 
-
-
 from PIL import Image
 from tqdm import tqdm
 from path_utils import PathUtils as p_utils
-
 
 
 ###################################################################################################
@@ -44,16 +41,16 @@ def generate_masked_image(image_path, mask_path):
     except Exception as e:
         print('')
         print('Ha ocurrido un error. No se puede cargar la imagen {}. EXCEPTION: {}'
-            .format(image_path, e))
+              .format(image_path, e))
         print('------------------------------------------------------------------------------------------')
         print(traceback.format_exc())
         return
-    try:                
+    try:
         bin_mask = np.array(Image.open(mask_path))
     except Exception as e:
         print('')
         print('Ha ocurrido un error. No se puede cargar la máscara {}. EXCEPTION: {}'
-            .format(mask_path, e))
+              .format(mask_path, e))
         print('------------------------------------------------------------------------------------------')
         print(traceback.format_exc())
         return
@@ -61,7 +58,6 @@ def generate_masked_image(image_path, mask_path):
     masked = cv2.bitwise_and(image, image, mask=bin_mask)
 
     return masked
-
 
 
 ###################################################################################################
@@ -83,7 +79,7 @@ def run(input_file_names, output_mask, output_masked):
         windows = True
     else:
         windows = False
-        
+
     if len(input_file_names) == 0:
         print("WARNING: No hay ficheros disponibles")
         return
@@ -97,7 +93,7 @@ def run(input_file_names, output_mask, output_masked):
         except Exception as e:
             print('')
             print('Ha ocurrido un error. No se ha podido abrir el fichero {}. EXCEPTION: {}'
-                .format(input_path, e))
+                  .format(input_path, e))
             print('------------------------------------------------------------------------------------------')
             print(traceback.format_exc())
             continue
@@ -118,9 +114,9 @@ def run(input_file_names, output_mask, output_masked):
         elapsed_time = time.time() - start_time
         print('')
         print('Aplicada máscara en imagen {} en {}.'
-            .format(image_file, humanfriendly.format_timespan(elapsed_time)))
+              .format(image_file, humanfriendly.format_timespan(elapsed_time)))
         time_infer.append(elapsed_time)
-        
+
         cv2.imwrite(output_path, masked)
 
     average_time_infer = statistics.mean(time_infer)
@@ -134,10 +130,8 @@ def run(input_file_names, output_mask, output_masked):
     print('==========================================================================================')
     print('De media, por cada imagen: ')
     print('Ha tomado {} en aplicar la máscara, con desviación de {}'
-        .format(humanfriendly.format_timespan(average_time_infer), std_dev_time_infer))
+          .format(humanfriendly.format_timespan(average_time_infer), std_dev_time_infer))
     print('==========================================================================================')
-
-
 
 
 ###################################################################################################
@@ -145,34 +139,34 @@ def run(input_file_names, output_mask, output_masked):
 
 def main():
     parser = argparse.ArgumentParser(
-        description = 'Módulo para aplicar las máscaras anteriormente generadas a las imágenes'
-            'indicadas'
-        )
-    group = parser.add_mutually_exclusive_group(required = True)
+        description='Módulo para aplicar las máscaras anteriormente generadas a las imágenes'
+                    'indicadas'
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument(
         '--json_file',
-        help = 'Fichero JSON del que se tomará la ruta de la imagen original a la que aplicar'
-            'la máscara'
+        help='Fichero JSON del que se tomará la ruta de la imagen original a la que aplicar'
+             'la máscara'
     )
     group.add_argument(
         '--json_dir',
-        help = 'Ruta al directorio donde se encuentran los ficheros JSON de los cuales se tomarán '
-            'las rutas de las imágenes a las que aplicar sus correspondientes máscaras, hace uso '
-            'de la opción --recursive'
+        help='Ruta al directorio donde se encuentran los ficheros JSON de los cuales se tomarán '
+             'las rutas de las imágenes a las que aplicar sus correspondientes máscaras, hace uso '
+             'de la opción --recursive'
     )
     parser.add_argument(
         '--recursive',
-        action = 'store_true',
-        help = 'Maneja directorios de forma recursiva, solo tiene sentido usarlo con --json_file'
+        action='store_true',
+        help='Maneja directorios de forma recursiva, solo tiene sentido usarlo con --json_file'
     )
     parser.add_argument(
         '--output_mask',
-        help = 'Ruta al directorio de donde se encuentran los ficheros de las máscaras '
+        help='Ruta al directorio de donde se encuentran los ficheros de las máscaras '
     )
     parser.add_argument(
         '--output_masked',
-        help = 'Ruta al directorio de donde se guardaran las imágenes una vez aplicadas sus máscaras'
-            'correspondientes'
+        help='Ruta al directorio de donde se guardaran las imágenes una vez aplicadas sus máscaras'
+             'correspondientes'
     )
 
     if len(sys.argv[1:]) == 0:
@@ -205,9 +199,9 @@ def main():
     print('')
     print('==========================================================================================')
     print('Aplicando máscaras en {} imágenes...'
-        .format(len(input_file_names)))
+          .format(len(input_file_names)))
     print('')
-    
+
     run(input_file_names=input_file_names, output_mask=args.output_mask, output_masked=args.output_masked)
 
     print('')
@@ -216,10 +210,7 @@ def main():
     print('==========================================================================================')
 
 
-
 if __name__ == '__main__':
-    main()    
+    main()
 
-
-
-###################################################################################################    
+    ###################################################################################################
