1fcd055f577e43987d71593858b78b956ef6ef36
Carlos García Silva <48029799+byLiTTo@users.noreply.github.com>
2024-09-10 19:31:02 +0200

    [FASE 1] - Creado notebook para convertir csv raw a dataset
    Creado nuevo notebook para convertir el dataset del fichero csv raw en un nuevo dataset de clases binarias guardado en un nuevo csv, además de unas estadisticas basicas para entender qué está ocurriendo con los datos manejados

--- src/data/Dataset.py
+++ src/data/Dataset.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.utils import shuffle
 
 
-def load_from_csv(path, sep=";", encoding="utf-8"):
+def load_from_csv(path: str, sep: str = ";", encoding: str = "utf-8") -> pd.DataFrame:
     """
     Load data from a CSV file.
 
@@ -21,7 +21,7 @@ def load_from_csv(path, sep=";", encoding="utf-8"):
     """
 
     try:
-        df = pd.read_csv(path, sep=sep)
+        df = pd.read_csv(path, sep=sep, encoding=encoding)
         print(f"The file {path} has been successfully opened.")
     except FileNotFoundError:
         print("The file does not exist.")
@@ -64,7 +64,10 @@ def convert_to_binary(
         dataset = dataset[
             dataset["label"].isin(negative_nomenclature + positive_nomenclature)
         ]
-        dataset["label"] = dataset["label"].apply(
+        # dataset["label"] = dataset["label"].apply(
+        #     lambda x: 0 if x in negative_nomenclature else 1
+        # )
+        dataset.loc[:, "label"] = dataset["label"].apply(
             lambda x: 0 if x in negative_nomenclature else 1
         )
     except Exception as e:
@@ -72,3 +75,24 @@ def convert_to_binary(
         return None
 
     return dataset
+
+
+def dataset_to_csv(
+    dataset: pd.DataFrame, csv_path: str, index: bool = False, sep: str = ";"
+) -> None:
+    """
+    Save the dataset to a CSV file.
+
+    Args:
+        dataset (pd.DataFrame): The dataset to be saved.
+        csv_path (str): The path to save the CSV file.
+        index (bool, optional): Whether to include the index in the CSV file. Defaults to False.
+        sep (str, optional): The delimiter to use in the CSV file. Defaults to ';'.
+    """
+    try:
+        dataset.to_csv(csv_path, index=index, sep=sep)
+        print(f"The dataset has been successfully saved to {csv_path}.")
+    except PermissionError:
+        print("You do not have permission to save the file.")
+    except Exception as e:
+        print(f"An error occurred while saving the dataset: {e}")
